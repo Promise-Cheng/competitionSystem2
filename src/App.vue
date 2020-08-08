633ead824affd324bb451572f86e97c99a02b540
cn <737362053@qq.com>
2020-08-08 18:37:15 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-
-
     <router-view v-if="$route.meta.keepAlive"></router-view>
     <keep-alive v-else>
-      <router-view ></router-view>
+      <router-view></router-view>
     </keep-alive>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-
-}
+  export default {
+    name: 'App',
+  }
 </script>
 
-<style  lang="scss">   //注意要用lang="scss"声明
-  @import "./style/weui";
+<style lang="scss">   //注意要用lang="scss"声明
+@import "./style/weui";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
